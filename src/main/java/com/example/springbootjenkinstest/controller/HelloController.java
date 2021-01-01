f3199847b44245e5110ba23c85a84e13eb550dad
cg <2890122341@qq.com>
2021-01-01 21:58:50 +0800

    创建HelloController

---
+++ src/main/java/com/example/springbootjenkinstest/controller/HelloController.java
@@ -0,0 +1,23 @@
+package com.example.springbootjenkinstest.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author: Cheng Gang
+ * @create_time: 2021/1/1 21:55
+ * @update_time:
+ * @description:
+ * @version: 0.0.1
+ */
+@RestController
+@RequestMapping("/")
+public class HelloController {
+
+    @GetMapping("hello")
+    public String hello() {
+        return "Hello Jenkins";
+    }
+}
