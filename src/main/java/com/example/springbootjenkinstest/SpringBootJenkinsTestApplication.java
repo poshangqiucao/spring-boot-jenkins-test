ec054eaed537ecc5215c232ccf9fe4d16475116d
cg <2890122341@qq.com>
2021-01-01 18:57:48 +0800

    初始化项目

---
+++ src/main/java/com/example/springbootjenkinstest/SpringBootJenkinsTestApplication.java
@@ -0,0 +1,16 @@
+package com.example.springbootjenkinstest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author cg
+ */
+@SpringBootApplication
+public class SpringBootJenkinsTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootJenkinsTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springbootjenkinstest/SpringBootJenkinsTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootjenkinstest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJenkinsTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
